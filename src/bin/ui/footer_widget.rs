3a0b7592f169b31a441e94216d68f89248797f6e
Nico Hormazabal <nhormazabal@newrelic.com>
2021-03-05 15:21:06 +0100

    fixes on ignore

--- src/bin/ui/footer_widget.rs
+++ src/bin/ui/footer_widget.rs
@@ -1,7 +1,4 @@
 use tui::{Frame, backend::Backend, layout::{Alignment, Rect}, style::{Color, Style}, widgets::{Block, BorderType, Borders, Paragraph, Widget}};
-use anyhow::Result;
-
-use super::RomstWidget;
 
 pub struct Footer {
 }
@@ -12,10 +9,8 @@ impl Footer {
 
         }
     }
-}
 
-impl <T: Backend> RomstWidget<T> for Footer {
-    fn render_in(&mut self, frame: &mut Frame<T>, area: Rect) {
+    fn render_in<T: Backend>(&self, frame: &mut Frame<T>, area: Rect) {
         let paragraph = Paragraph::new("Romst, a Rom checker written in Rust (press 'q' to quit)")
             .style(Style::default().fg(Color::LightCyan))
             .alignment(Alignment::Center)
@@ -27,9 +22,5 @@ impl <T: Backend> RomstWidget<T> for Footer {
                     .border_type(BorderType::Plain),
             );
         frame.render_widget(paragraph, area);
-    }
-
-    fn process_key(&mut self, event: crossterm::event::KeyEvent) {
-
     }
 }
